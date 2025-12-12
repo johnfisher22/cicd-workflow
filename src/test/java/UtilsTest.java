32437cf3f853ae4d96f1fe830e19451d9ea19c20
Johnfisher <simplefisher@gmail.com>
2025-12-11 21:24:02 -0600

    Update UtilsTest.java

--- src/test/java/UtilsTest.java
+++ src/test/java/UtilsTest.java
@@ -1,1 +1,35 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
+public class UtilsTest {
+
+    @Test
+    void testCheckNameValid() {
+        assertTrue(Utils.checkName("John"));
+    }
+
+    @Test
+    void testCheckNameEmpty() {
+        assertFalse(Utils.checkName(""));
+    }
+
+    @Test
+    void testCheckNameNull() {
+        assertFalse(Utils.checkName(null));
+    }
+
+    @Test
+    void testIsValidAgeValid() {
+        assertTrue(Utils.isValidAge(20));
+    }
+
+    @Test
+    void testIsValidAgeNegative() {
+        assertFalse(Utils.isValidAge(-1));
+    }
+
+    @Test
+    void testIsValidAgeTooLargeBugAllows120Plus() {
+        assertTrue(Utils.isValidAge(150));  // This exposes bug
+    }
+}
